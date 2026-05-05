ecace578a993258acc67fe8125b94e061f42208c
Mirthipati Nagendra <143808246+NAGENDRA2866@users.noreply.github.com>
2026-05-05 12:15:12 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const User = require("./models/user");
 
+const authRoutes = require("./routes/auth");
+const reviewRoutes = require("./routes/reviews");
 
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://127.0.0.1:27017/reviewApp");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+
+app.use("/", require("./routes/auth"));
+app.use("/reviews", require("./routes/reviews"));
+
+app.get("/", (req, res) => {
+  res.redirect("/reviews");
+});
 
 const PORT = 5000;
 app.listen(PORT,()=>{
