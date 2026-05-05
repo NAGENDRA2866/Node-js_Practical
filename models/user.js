a77dcc273cde87706aab4bdc223cddf06802e4bc
Mirthipati Nagendra <143808246+NAGENDRA2866@users.noreply.github.com>
2026-05-05 11:58:28 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  favoriteGenre: { type: String, required: true },
+  bio: String,
+  reviewerSince: { type: Date, default: Date.now }
+});
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model("User", userSchema);
