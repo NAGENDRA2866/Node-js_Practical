40e635d21f9eee0542dd1dd6cd521db8b87b3303
Mirthipati Nagendra <143808246+NAGENDRA2866@users.noreply.github.com>
2026-05-05 12:38:04 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -7,11 +6,6 @@ const userSchema = new mongoose.Schema({
   favoriteGenre: { type: String, required: true },
   bio: String,
   reviewerSince: { type: Date, default: Date.now }
-});
-
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, 10);
 });
 
 module.exports = mongoose.model("User", userSchema);
